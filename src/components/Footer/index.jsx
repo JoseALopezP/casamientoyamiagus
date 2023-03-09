8cd3653e202d62843e5ebcec81ba979509cf667b
JoseALopezP <jalp42@live.com>
2023-03-08 21:59:25 -0300

    Merged

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,14 +5,14 @@ const Footer = () => {
     return (
         <div className='footerBlock'>
             <div className="footerMsg">
-                <h5 className='greenPart'> Por favor, recordá confirmar tu asistencia antes del 17 de noviembre</h5>
+                <h5 className='greenPart'> Por favor, recordá confirmar tu asistencia antes del 24 de Marzo</h5>
                 <p className='greenPart'>¡Te esperamos para compartir este momento tan importante de nuestras vidas! </p>
                 <p className='goldenPart'>Yami & Agus</p>
             </div>
             <div className="contactInfo">
                 <p className='contactMsg'>Por cualquier duda o modificación, podés contactarte con cualquiera de nosotros a nuestros números de celular:</p>
-                <p className='contactGabi'>264-5475366 (Yami)</p>
-                <p className='contactLucas'>264-5868113 (Agus)</p>
+                <p className='contactGabi'>264-6702241 (Yami)</p>
+                <p className='contactLucas'>264-4836195 (Agus)</p>
             </div>
         </div>
     );
